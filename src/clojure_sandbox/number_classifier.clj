b8050e6765ea230ce879c359f5789931332d4c80
Martin Rist <mlr-github@martinrist.com>
2013-11-10 21:56:25 +0000

    Basic Clojure implementation of Number Classifier from Neal Ford's 'Functional Thinking'

---
+++ src/clojure_sandbox/number_classifier.clj
@@ -0,0 +1,21 @@
+(ns clojure-sandbox.number_classifier)
+
+(defn factor? [number x]
+  (zero? (mod number x)))
+
+(defn factor-of-number? [number]
+    (fn [x] (factor? number x)))
+
+; TODO: Work out a way of using partial to improve this and remove the need for factor-of-number
+(defn factors [number]
+ (filter (factor-of-number? number) (range 1 number)))
+
+(defn factors [number]
+ (filter (factor? number %) (range 1 number)))
+
+
+(defn sum-factors [number]
+  (reduce + (factors number)))
+
+(defn perfect? [number]
+  (= (sum-factors number) number))
