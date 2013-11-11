b9ae13a32de6b3d54ae7ee31c3b9e3c748f9175d
Martin Rist <martin.rist@flybe.com>
2013-11-11 12:44:17 +0000

    Replace factor-of-number with implementation using partial

--- src/clojure_sandbox/number_classifier.clj
+++ src/clojure_sandbox/number_classifier.clj
@@ -3,12 +3,8 @@
 (defn factor? [number x]
   (zero? (mod number x)))
 
-(defn factor-of-number? [number]
-    (fn [x] (factor? number x)))
-
-; TODO: Work out a way of using partial to improve this and remove the need for factor-of-number
 (defn factors [number]
- (filter (factor-of-number? number) (range 1 number)))
+ (filter (partial factor? number) (range 1 number)))
 
 (defn sum-factors [number]
   (reduce + (factors number)))
