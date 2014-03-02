77c9dc10db0e141d641f067a58d1cdfee202dcf5
Martin Rist <mlr-github@martinrist.com>
2014-03-02 13:58:05 +0000

    Completed examples from 'Programming Clojure' chapter 3

--- src/programming_clojure/sequences.clj
+++ src/programming_clojure/sequences.clj
@@ -1,4 +1,9 @@
-(ns programming-clojure.sequences)
+(ns programming-clojure.sequences
+  (:require [clojure.java.io :as io]
+            [clojure.string :as string]
+            [clojure.xml :as xml]
+            [clojure.set :refer :all])
+  (:import (java.io StringReader File)))
 
 (def my-list '(1 2 3 4 5))
 (def my-vec [1 2 3 4 5])
@@ -6,10 +11,17 @@
 (def my-sorted-set (into (sorted-set) my-set))
 (def my-map {:a 1 :b 2 :c 3 :d 4 :e 5})
 
+(def languages #{"java" "c" "d" "clojure"})
+(def beverages #{"java" "chai" "pop"})
+
 (def integers (iterate inc 0))
 
 (defn integers-less-than
   [n]
   (take-while #(< % n) integers))
 
-()
+(def text "The quick brown fox jumps over the lazy dog")
+(def words (clojure.string/split text #"\W+"))
+
+;(def xml (xml/parse (File. "src/programming_clojure/compositions.xml")))
+(def xml (xml/parse "src/programming_clojure/compositions.xml" ))
