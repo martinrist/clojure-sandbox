7ede4130d99778d871dd10406a22ca09da85ba73
Martin Rist <mlr-github@martinrist.com>
2014-03-09 15:27:37 +0000

    Add examples of lazy and parallel compilation, together with nasty side-effects to demonstrate what's happening

--- src/clojure_sandbox/prismatic.clj
+++ src/clojure_sandbox/prismatic.clj
@@ -21,13 +21,15 @@
 (declare xs n m m2)   ; for IntelliJ
 
 (def stats-graph
-  {:n    (fnk [xs]    (count xs))
-   :m    (fnk [xs n]  (/ (sum identity xs) n))
-   :m2   (fnk [xs n]  (/ (sum #(* % %) xs) n))
-   :v    (fnk [m m2]  (- m2 (* m m)))
+  {:n    (fnk [xs]    (doseq [] (println (str "Calculating n in thread " (Thread/currentThread))) (count xs)))
+   :m    (fnk [xs n]  (doseq [] (println (str "Calculating m in thread " (Thread/currentThread))) (/ (sum identity xs) n)))
+   :m2   (fnk [xs n]  (doseq [] (println (str "Calculating m2 in thread " (Thread/currentThread))) (/ (sum #(* % %) xs) n)))
+   :v    (fnk [m m2]  (doseq [] (println (str "Calculating v in thread " (Thread/currentThread))) (- m2 (* m m))))
    })
 
 (def stats-eager (graph/eager-compile stats-graph))
+(def stats-lazy (graph/lazy-compile stats-graph))
+(def stats-parallel (graph/par-compile stats-graph))
 
 (def extended-stats-graph
   (assoc stats-graph :sd (fnk [v] (Math/sqrt v))))
