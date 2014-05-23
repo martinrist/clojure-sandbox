63e35ef54fbe11d4672508244241ec65c521dcde
Martin Rist <martin.rist@flybe.com>
2014-05-23 15:44:55 +0100

    Tests to illustrate how refs work

---
+++ test/programming_clojure/state_test.clj
@@ -0,0 +1,59 @@
+(ns clojure-sandbox.state-test
+  (:require [clojure.test :refer :all]))
+
+(deftest ref-tests
+  (testing "Cannot update refs outside a transaction"
+    (let [r (ref nil)]
+      (is (thrown? IllegalStateException (ref-set r 1)))
+      (is (nil? @r))))
+
+  (testing "Can update refs in a transaction"
+    (let [r (ref nil)]
+      (dosync
+        (ref-set r 1))
+      (is (= 1 @r))))
+
+  (testing "Multiple ref updates in a transaction"
+    (let [r (ref nil)]
+      (dosync
+        (ref-set r 1)
+        (ref-set r 2))
+      (is (= 2 @r))))
+
+  (testing "Throwing exception in dosync prevents ref udpate"
+    (let [r (ref nil)]
+      (is (thrown? Exception (dosync
+                               (ref-set r 1)
+                               (throw (Exception.)))))
+      (is (nil? @r))))
+
+  (testing "Using alter to update ref based on existing value"
+    (let [counter (ref 0)]
+      (dosync
+        (alter counter inc))
+      (is (= 1 @counter))))
+
+  (testing "Many incrementing alters in separate transactions don't corrupt state"
+    (let [r (ref 0)
+          iterations 1000000]
+      (->> (for [x (range iterations)]
+             (future (dosync (alter r inc))))
+           (map deref)
+           doall)
+      (is (= iterations @r))))
+
+  (testing "Ref with validator function"
+    (let [r (ref 0 :validator #(< % 100))]
+
+      (testing "Update to < 100 works"
+        (dosync (ref-set r 99))
+        (is (= @r 99)))
+
+      #_(testing "Update to >= 100 fails validation and doesn't change ref"
+        (is (thrown? IllegalStateException) (dosync (ref-set r 100)))
+        (is (= @r 99)))
+
+
+      ))
+
+  )
