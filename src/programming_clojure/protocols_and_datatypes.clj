2e1da2b16b2e5d991f41ed1336e14de6ce80433c
Martin Rist <martin.rist@flybe.com>
2014-06-06 11:00:09 +0100

    Initial implementation of `gulp` and `expectorate` from Chapter 6 of 'Programming Clojure'

---
+++ src/programming_clojure/protocols_and_datatypes.clj
@@ -0,0 +1,28 @@
+(ns programming-clojure.protocols-and-datatypes
+  (:import (java.lang StringBuilder)
+           (java.io FileInputStream InputStreamReader BufferedReader FileOutputStream
+                    OutputStreamWriter BufferedWriter)))
+
+(defn gulp
+  [src]
+  (let [sb (StringBuilder.)]
+    (with-open [reader (-> src
+                           FileInputStream.
+                           InputStreamReader.
+                           BufferedReader.)]
+      (loop [c (.read reader)]
+        (if (neg? c)
+          (str sb)
+          (do
+            (.append sb (char c))
+            (recur (.read reader))))))))
+
+(defn expectorate
+  [dst content]
+  (with-open [writer (-> dst
+                         FileOutputStream.
+                         OutputStreamWriter.
+                         BufferedWriter.
+                         )]
+    (.write writer (str content))))
+
