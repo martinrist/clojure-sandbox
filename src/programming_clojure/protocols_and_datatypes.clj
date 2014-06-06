3f6f27c533f751861b002af0e6599f6cc3e74863
Martin Rist <martin.rist@flybe.com>
2014-06-06 11:14:15 +0100

    Horrific first version of implementing support for multiple reader / writer types.  Subsequent iterations will improve this...

--- src/programming_clojure/protocols_and_datatypes.clj
+++ src/programming_clojure/protocols_and_datatypes.clj
@@ -1,19 +1,33 @@
 (ns programming-clojure.protocols-and-datatypes
   (:import (java.lang StringBuilder)
            (java.io FileInputStream InputStreamReader BufferedReader FileOutputStream
-                    OutputStreamWriter BufferedWriter)))
+                    OutputStreamWriter BufferedWriter)
+           (java.net Socket URL)))
 
 (defn make-reader
   [src]
-  (-> src
-      FileInputStream.
+  (-> (condp = (type src)
+        java.io.InputStream src
+        java.lang.String (FileInputStream. src)
+        java.io.File (FileInputStream. src)
+        java.net.Socket (.getInputStream src)
+        java.net.URL (if (= "file" (.getProtocol src))
+                       (-> src .getPath FileInputStream.)
+                       (.openStream src)))
       InputStreamReader.
       BufferedReader.))
 
 (defn make-writer
-  [src]
-  (-> src
-      FileOutputStream.
+  [dst]
+  (-> (condp = (type dst)
+        java.io.OutputStream dst
+        java.lang.String (FileOutputStream. dst)
+        java.io.File (FileOutputStream. dst)
+        java.net.Socket (.getOutputStream dst)
+        java.net.URL (if (= "file" (.getProtocol dst))
+                       (-> dst .getPath FileOutputStream.)
+                       (throw (IllegalArgumentException.
+                                "Can't write to non-file URL"))))
       OutputStreamWriter.
       BufferedWriter.))
 
