b0e8391fa18a492274557e07e9a8448a4c75157f
Martin Rist <martin.rist@flybe.com>
2014-06-06 13:55:57 +0100

    Refactor to use protocols properly

--- src/programming_clojure/protocols_and_datatypes.clj
+++ src/programming_clojure/protocols_and_datatypes.clj
@@ -1,35 +1,54 @@
 (ns programming-clojure.protocols-and-datatypes
   (:import (java.lang StringBuilder)
            (java.io FileInputStream InputStreamReader BufferedReader FileOutputStream
-                    OutputStreamWriter BufferedWriter)
+                    OutputStreamWriter BufferedWriter InputStream OutputStream File)
            (java.net Socket URL)))
 
-(defn make-reader
-  [src]
-  (-> (condp = (type src)
-        java.io.InputStream src
-        java.lang.String (FileInputStream. src)
-        java.io.File (FileInputStream. src)
-        java.net.Socket (.getInputStream src)
-        java.net.URL (if (= "file" (.getProtocol src))
-                       (-> src .getPath FileInputStream.)
-                       (.openStream src)))
-      InputStreamReader.
-      BufferedReader.))
+(defprotocol IOFactory
+  "A protocol for things that can be read from and written to."
+  (make-reader [this] "Creates a BufferedReader.")
+  (make-writer [this] "Creates a BufferedWriter."))
 
-(defn make-writer
-  [dst]
-  (-> (condp = (type dst)
-        java.io.OutputStream dst
-        java.lang.String (FileOutputStream. dst)
-        java.io.File (FileOutputStream. dst)
-        java.net.Socket (.getOutputStream dst)
-        java.net.URL (if (= "file" (.getProtocol dst))
-                       (-> dst .getPath FileOutputStream.)
-                       (throw (IllegalArgumentException.
-                                "Can't write to non-file URL"))))
-      OutputStreamWriter.
-      BufferedWriter.))
+(extend InputStream
+  IOFactory
+  {:make-reader (fn [src] (-> src InputStreamReader. BufferedReader.))
+   :make-writer (fn [_] (throw (IllegalArgumentException. "Can't open as an InputStream")))})
+
+(extend OutputStream
+  IOFactory
+  {:make-reader (fn [_] (throw (IllegalArgumentException. "Can't open as an OutputStream")))
+   :make-writer (fn [dst] (-> dst OutputStreamWriter. BufferedWriter.))})
+
+(extend-type File
+  IOFactory
+  (make-reader [src]
+    (make-reader (FileInputStream. src)))
+  (make-writer [dst]
+    (make-writer (FileOutputStream. dst))))
+
+(extend-protocol IOFactory
+  Socket
+  (make-reader [src]
+    (make-reader (.getInputStream src)))
+  (make-writer [dst]
+    (make-writer (.getOutputStream dst)))
+
+  URL
+  (make-reader [src]
+    (if (= "file" (.getProtocol src))
+      (-> src .getPath FileInputStream.)
+      (.openStream src)))
+  (make-writer [dst]
+    (if (= "file" (.getProtocol dst))
+      (-> dst .getPath FileOutputStream.)
+      (throw (IllegalArgumentException.
+               "Can't write to non-file URL"))))
+
+  String
+  (make-reader [src]
+    (make-reader (FileInputStream. src)))
+  (make-writer [dst]
+    (make-writer (FileOutputStream. dst))))
 
 (defn gulp
   [src]
@@ -46,5 +65,3 @@
   [dst content]
   (with-open [writer (make-writer dst)]
     (.write writer (str content))))
-
-
