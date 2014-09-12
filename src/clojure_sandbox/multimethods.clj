c42968f1d275c139fb075a7fe09f0e9ea39d01e0
Martin Rist <martin.rist@flybe.com>
2014-09-12 14:12:46 +0100

    Add multimethod examples from 'The Joy of Clojure'

---
+++ src/clojure_sandbox/multimethods.clj
@@ -0,0 +1,38 @@
+(ns clojure-sandbox.multimethods
+  (:refer-clojure :exclude [get]))
+
+(defn beget
+  [this proto]
+  (assoc this ::prototype proto))
+
+(defn get
+  [m k]
+  (when m
+    (if-let [[_ v] (find m k)]
+      v
+      (recur (::prototype m) k))))
+
+
+
+(def unix {:os ::unix :c-compiler "cc" :home "/home" :dev "/dev"})
+(def osx (beget {:os ::osx :llvm-compiler "clang" :home "/Users"} unix))
+
+(defmulti compiler :os)
+(defmethod compiler ::unix
+           [m]
+           (get m :c-compiler))
+(defmethod compiler ::osx
+           [m]
+           (get m :llvm-compiler))
+
+(defmulti home :os)
+(defmethod home ::unix
+           [m]
+           (get m :home))
+(defmethod home ::bsd
+           [m]
+  (str "bsd -> /home"))
+
+
+(derive ::osx ::unix)
+(derive ::osx ::bsd)
