889b17583de3839488853329856f26643f057c90
Martin Rist <martin.rist@flybe.com>
2014-10-10 15:44:02 +0100

    Add reducers examples from 'The Joy of Clojure'

---
+++ src/joy_of_clojure/reducers.clj
@@ -0,0 +1,49 @@
+(ns clojure-sandbox.joy-of-clojure.reducers)
+
+(defn empty-range?
+  [start end step]
+  (or (and (pos? step) (>= start end))
+      (and (neg? step) (<= start end))))
+
+(defn lazy-range
+  "Returns a lazy seq equivalent to `(range start end step)`"
+  [start end step]
+  (lazy-seq
+    (if (empty-range? start end step)
+      nil
+      (cons start
+            (lazy-range (+ start step)
+                        end
+                        step)))))
+
+(defn reducible-range
+  "Reducible constructor that creates a reducible with the
+  same values as `(range start end step)`."
+  [start end step]
+  (fn [reducing-fn initial-value]
+    (loop [result initial-value
+           i      start]
+      (if (empty-range? i end step)
+        result
+        (recur (reducing-fn result i)
+               (+ i step))))))
+
+(defn half
+  "Mapping function that halves values"
+  [x]
+  (/ x 2))
+
+(defn half-transformer
+  "Reducing function transformer that takes `reducing-fn` and
+  returns another reducing function that pre-applies `half`"
+  [reducing-fn]
+  (fn [result input]
+    (reducing-fn result (half input))))
+
+(defn mapping
+  "Transformer constructor that returns a reducing function
+  transformer that applies `map-fn`."
+  [map-fn]
+  (fn map-transform [reducing-fn]
+    (fn [result input]
+      (reducing-fn result (map-fn input)))))
