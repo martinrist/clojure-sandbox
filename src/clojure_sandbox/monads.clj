9af1dd468844743253f2707b033a7ffd6538deba
Martin Rist <martin.rist@flybe.com>
2015-03-06 13:58:27 +0000

    Complete part 1 of Monad Tutorial (https://github.com/khinsen/monads-in-clojure/blob/master/PART1.md)

--- src/clojure_sandbox/monads.clj
+++ src/clojure_sandbox/monads.clj
@@ -2,8 +2,11 @@
   (:require [clojure.algo.monads :refer :all]))
 
 
+; For IntelliJ
+(declare a b)
 
-; Part 1 - https://github.com/khinsen/monads-in-clojure/blob/master/PART1.md
+
+;; Part 1 - https://github.com/khinsen/monads-in-clojure/blob/master/PART1.md
 
 ; Example of a simple 'let' binding.
 ; First 1 is bound to `a`, then `inc a` is bound to `b`, then `(* a b)` is
@@ -11,7 +14,7 @@
 
 (let [a 1
       b (inc a)]
-  (* a b))
+  (* a b))                                                  ; (1)
 
 
 
@@ -21,36 +24,93 @@
 
 ((fn [a]
    ((fn [b]
-      (* a b)) (inc a))) 1)
+      (* a b)) (inc a))) 1)                                 ; (2)
 
 
 
 ; In a first attempt to clean this up, define a helper function 'bind'
 ; that 'reverses' a value and function:
 
-(defn bind
+(defn m-bind-identity
   [val f]
-  (f val))
+  (f val))                                                  ; (3)
 
 
 
 ; This allows us to rewrite the earlier example as something that looks
-; a little closer to the nice 'let' form:
+; a little closer to the nice 'let' form (1).
 
-(bind 1       (fn [a]
-(bind (inc a) (fn [b]
-      (* a b)))))
-
-
+(m-bind-identity 1       (fn [a]
+(m-bind-identity (inc a) (fn [b]
+                 (* a b)))))                                ; (4)
 
 
 
-
+; Finally, we use the `domonad` macro to turn this into something that
+; looks almost exactly like the `let` form (1)
+; `identity-m` is the 'Identity' monad, defined in `clojure.algo.monads`
 
 (domonad identity-m
          [a 1
           b (inc a)]
-         (* a b))
+         (* a b))                                           ; (5)
+
+
+
+; Using macroexpand-1 on (5) gives:
+(clojure.algo.monads/with-monad identity-m
+    (m-bind   1       (fn [a]
+    (m-bind   (inc a) (fn [b]
+    (m-result (* a b)))))))                                 ; (6)
+
+; which is almost exactly (4), with the following differences:
+;
+; - It's run within `with-monad identity-m` to specify evaluation within the identity monad.
+; - s/bind/m-bind/, since we're using the m-bind operation from identity-m
+; - Addition of `m-result`, which for identity-m is just `identity`
+
+; So, monads are generalisations of the 'let' form that replace the simple
+; m-bind and m-result functions with something more complex.  Each monad is
+; defined by an implemetnation of m-bind and m-result.
+
+
+
+; Next example - suppose we have computations that can fail, signalling failure
+; by returning `nil`.  So, if the result of any of the following steps is `nil`
+; (e.g. x is nil), the result is `nil` and no subsequent steps are run.
+
+(defn f
+  [x]
+  (let [a x
+        b (inc a)]
+    (* a b)))
+
+(f 1)        ; => 2
+(f nil)      ; => Null Pointer Exception - we want nil
+
+
+
+; Here we can use the 'Maybe' monad
+
+(defn f'
+  [x]
+  (domonad maybe-m
+           [a   x
+            b   (inc a)]
+           (* a b)))
+
+(f' 1)        ; => 2, as before
+(f' nil)      ; => nil, as required
+
+
+
+; The m-bind function for maybe-m is:
+
+(defn m-bind-maybe
+  [val f]
+  (if (nil? val)
+    nil
+    (f val)))
 
 
 
