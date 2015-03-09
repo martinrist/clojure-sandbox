c95ebaed9e0da5df769730dc37aa922366860649
Martin Rist <martin.rist@flybe.com>
2015-03-09 09:03:23 +0000

    More work on validation, moving towards using err->> macro to chain validations

--- src/clojure_sandbox/validation.clj
+++ src/clojure_sandbox/validation.clj
@@ -60,3 +60,91 @@
       (validate-size 5)
       (validate-contents pos?)
       (println coll)))
+
+(defn validate-not-empty
+  "Validates that `coll` is not empty"
+  [coll]
+  (println "Checking collection is not empty")
+  (if (empty? coll)
+    [nil "Collection cannot be empty"]
+    [coll nil]))
+
+(defn validate-size
+  "Validate that the size of `coll` is not more than `limit`"
+  [limit coll]
+  (println "Checking collection is not too large")
+  (let [size (count coll)]
+    (if (> size limit)
+      [nil (str "Collection has more elements than allowed (" size ">" limit ")")]
+      [coll nil])))
+
+(defn validate-contents
+  "Validate that all the members of `coll` satisfy `pred`"
+  [pred coll]
+  (println "Checking members of collection")
+  (if (every? pred coll)
+    [coll nil]
+    [nil "Validation failed for a member of the collection"]))
+
+(defn validate-and-perform
+  "First attempt at chaining validations together.  Note the repeated pattern
+  of (if (nil? err) (...) [nil err]"
+  [coll]
+  (let [[coll err] (validate-not-empty coll)
+        [coll err] (if (nil? err) (validate-size 5 coll) [nil err])
+        [coll err] (if (nil? err) (validate-contents pos? coll) [nil err])]
+    (if (nil? err)
+      (println "Collection passed all validation: " coll)
+      (println "Validation failed: " err))))
+
+(defn bind-error
+  [f [val err]]
+  (if (nil? err)
+    (f val)
+    [nil err]))
+
+(defn validate-and-perform
+  "Second attempt at chaining validation, after pulling out apply-or-error"
+  [coll]
+  (let [result (validate-not-empty coll)
+        result (bind-error (partial validate-size 5) result)
+        [val err] (bind-error (partial validate-contents pos?) result)]
+    (if (nil? err)
+      (println "Collection passed all validation: " val)
+      (println "Validation failed: " err))))
+
+
+(defn validate-and-perform
+  "Third attempt at chaining validation - as above but using ->>"
+  [coll]
+  (let [[val err] (->> coll
+                       validate-not-empty
+                       (bind-error (partial validate-size 5))
+                       (bind-error (partial validate-contents pos?)))]
+    (if (nil? err)
+      (println "Collection passed all validation: " val)
+      (println "Validation failed: " err))))
+
+
+; Holy shit - here come the macros
+(defmacro err->>
+  [val & fns]
+  (let [fns (for [f fns] `(bind-error ~f))]
+    `(->> [~val nil]
+          ~@fns)))
+
+
+(defn validate-and-perform
+  "Fourth attempt at chaining validation using err->>"
+  [coll]
+  (let [[val err] (err->> coll
+                          validate-not-empty
+                          (partial validate-size 5)
+                          (partial validate-contents pos?))]
+    (if (nil? err)
+      (println "Collection passed all validation: " val)
+      (println "Validation failed: " err))
+
+    )
+
+  )
