0c8ffe831feb225d2d24b467a530ebbf45cff04f
Martin Rist <mlr-github@martinrist.com>
2015-11-11 17:53:09 +0000

    Upgrade to Clojure 1.7

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.6.0"]
+  :dependencies [[org.clojure/clojure "1.7.0"]
                  [prismatic/plumbing "0.2.0"]
                  [incanter/incanter "1.5.5"]
                  [org.clojure/algo.monads "0.1.5"]]
