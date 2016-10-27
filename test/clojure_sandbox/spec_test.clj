84a6abf434c617ae1406aeda62a8461877bb026b
Martin Rist <mlr-github@martinrist.com>
2016-10-27 12:23:32 +0100

    Add examples on `explain`, `explain-str` and `explain-data`

--- test/clojure_sandbox/spec_test.clj
+++ test/clojure_sandbox/spec_test.clj
@@ -20,12 +20,11 @@
       (is (s/valid? suits :hearts))
       (is (not (s/valid? suits :frogs))))))
 
+; Use `s/def` to define a registry of some basic specs
+(s/def ::date inst?)
+(s/def ::suit #{:heart :club :diamond :spade})
 
 (deftest registry "http://clojure.org/guides/spec#_registry"
-
-  ; Use `s/def` to define a registry of some basic specs
-  (s/def ::date inst?)
-  (s/def ::suit #{:heart :club :diamond :spade})
 
   (testing "using basic registry entries as specs"
     (is (s/valid? ::date (Date.)))
@@ -49,7 +48,31 @@
     (is (not (s/valid? ::name-or-id :foo)))
 
     (is (= (s/conform ::name-or-id "abc") [:name "abc"])
-        "Conforming a spec created with `or` returns a vector describing conformation"))
+        "Conforming a spec created with `or` returns a vector describing conformation")))
 
 
-  )
+(deftest explain "http://clojure.org/guides/spec#_explain"
+
+   (testing "using `explain` to report conformation to *out*"
+     (is (= (.trim (with-out-str (s/explain ::suit :club))) "Success!"))
+     (is (-> (s/explain ::suit 42)
+              with-out-str
+              (.contains "val: 42 fails spec"))))
+
+   (testing "using `explain-str` to avoid the need for with-out-str"
+     (is (= (.trim (s/explain-str ::suit :club)) "Success!")))
+
+   (testing "using `explain-data` to get a data structure back"
+     (let [result (s/explain-data ::name-or-id :foo)]
+       (is (map? result) "Result of validation is a map")
+       (is (contains? result :clojure.spec/problems) "Result contains `:problems` key")
+       (let [problems (:clojure.spec/problems result)]
+         (is (= (count problems) 2) "Result contains two problems")
+         (is (= (-> problems
+                    first
+                    :path
+                    first) :name) "First problem is in the `:name` path")
+         (is (= (-> problems
+                    second
+                    :path
+                    first) :id) "Second problem is in the `:id` path")))))
